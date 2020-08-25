32302534a61c58df9606921f73b47cb9105acff4
donal <donalspring@gmail.com>
2020-08-25 15:14:27 +0100

    🐮 INIT - System test using Zalenium 🥛

---
+++ cucumber.conf.js
@@ -0,0 +1,54 @@
+const {
+  setDefaultTimeout,
+  AfterAll,
+  BeforeAll
+} = require('cucumber');
+const {
+  createSession,
+  closeSession,
+  startWebDriver,
+  stopWebDriver,
+} = require('nightwatch-api');
+
+const reporter = require('cucumber-html-reporter');
+
+setDefaultTimeout(60000);
+
+BeforeAll(async () => {
+  if (!process.env.CI) {
+    console.info("Running in LOCAL mode")
+    await startWebDriver();
+    await createSession({
+      env: 'default',
+      configFile: 'nightwatch.conf.js',
+      silent: false,
+    });
+  } else {
+    console.info("Running in CI mode")
+    await createSession({
+      env: 'jenkins',
+      configFile: 'nightwatch.conf.ci.js',
+      silent: false,
+    });
+  }
+});
+
+AfterAll(async () => {
+  await closeSession();
+  if (!process.env.CI) {
+    await stopWebDriver();
+  }
+  setTimeout(() => {
+    reporter.generate({
+      theme: 'bootstrap',
+      jsonFile: 'reports/cucumber_report.json',
+      output: 'reports/cucumber_report.html',
+      reportSuiteAsScenarios: true,
+      launchReport: false,
+      metadata: {
+        'App': 'LXP System Tests',
+        'Environment': 'labs-test'
+      }
+    });
+  }, 1000);
+});
---
+++ nightwatch.conf.ci.js
@@ -0,0 +1,20 @@
+module.exports = {
+  src_folders: ['e2e'], //tests is a folder in workspace which has the step definitions
+  test_settings: {
+    jenkins: {
+      selenium_host: 'zalenium-labs-ci-cd.apps.who.emea-2.rht-labs.com',
+      selenium_port: 80,
+      use_ssl: false,
+      end_session_on_fail: false,
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        'goog:chromeOptions': {
+          w3c: false,
+          args: ['--disable-web-security', '--ignore-certificate-errors'],
+        },
+      },
+    },
+  },
+};
